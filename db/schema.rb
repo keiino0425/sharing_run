d45e8de97b9435f2315b0b57b46b8fad29202f5a
keiino0425 <keiino0425@gmail.com>
2022-05-06 22:16:04 +0000

    eventモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_214502) do
+ActiveRecord::Schema.define(version: 2022_05_06_221445) do
+
+  create_table "events", charset: "utf8", force: :cascade do |t|
+    t.string "event_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
